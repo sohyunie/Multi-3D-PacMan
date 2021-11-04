7558d481e73aff33274d7079b8bfdaa48578699c
Tomson0119 <kjs980119@gmail.com>
2021-11-04 17:38:38 +0900

    refactor - 예외처리 클래스 정의 및 구현

--- NetworkLibrary/NetworkLibrary/Exception.cpp
+++ NetworkLibrary/NetworkLibrary/Exception.cpp
@@ -1,11 +1,21 @@
 #include "Exception.h"
+#include "Protocol.h"
 
-Exception::Exception(int error_code)
+Exception::Exception(const char* error_msg)
 {
-	// errorStr º¯È¯
+	LPSTR msgBuffer;
+	size_t size = FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPSTR)&msgBuffer, 0, NULL);
+
+	ErrorString = error_msg;
+	ErrorString += ": ";
+	ErrorString += msgBuffer;
 }
 
 const char* Exception::what()
 {
-	return ErrorString;
+	return ErrorString.c_str();
 }
