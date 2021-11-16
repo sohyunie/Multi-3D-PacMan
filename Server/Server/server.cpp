2a569a1bb6b68da6f7bef6f78c39208d0c5b86b3
Tomson0119 <kjs980119@gmail.com>
2021-11-16 11:56:05 +0900

    change char to int

--- Server/Server/server.cpp
+++ Server/Server/server.cpp
@@ -205,7 +205,7 @@ vector<object_status> Server::UpdateObjectStatus(int id)
 			if (IsCollided(clientBB, beadBB))
 			{
 				bead.active = false;
-				obj_stats.push_back( bead.type , bead.id, bead.active );
+				obj_stats.push_back({ bead.type , bead.id, bead.active });
 			}
 		}
 	}
@@ -218,7 +218,7 @@ vector<object_status> Server::UpdateObjectStatus(int id)
 			{
 				key.active = false;
 				m_countOfKeyAccquired += 1;
-				obj_stats.emplace_back(key.type, key.id, key.active);
+				obj_stats.push_back({ key.type, key.id, key.active });
 			}
 		}
 	}
